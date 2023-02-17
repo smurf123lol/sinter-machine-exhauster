6b278d495c5410fe9e5d5ce0975f7d2a622aa8ea
smurf123lol <znamenshchikov2000@gmail.com>
2023-02-17 22:06:43 +0500

    React project

---
+++ react-app/src/Components/ExhausterComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class ExhausterComponent extends React.Component{
+    constructor(props){
+        super();
+    }
+    render(){
+        return <div className="exhauster-component"></div>
+    }
+}
+
+export default ExhausterComponent;
